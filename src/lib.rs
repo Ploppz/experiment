0c178ef44a4ddee56162e3f3d068a24145be5eda
Erlend Langseth <3rlendhl@gmail.com>
2019-03-11 16:28:27 +0100

    Give ContinuousView to this crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,45 +3,44 @@ use serde::{Serialize, de::DeserializeOwned, Deserialize};
 use chrono::prelude::*;
 use failure::Error;
 use ndarray::{Array1, Array2};
+use plotlib::{
+    repr::{Line, LineStyle},
+    view::ContinuousView,
+};
 
 
 pub struct Page<'a> {
-    plots: Vec<Plot<'a>>,
-    x_label: &'a str,
-    y_label: &'a str,
+    plots: Vec<Plot>,
+    config: ContinuousView,
     name: &'a str,
 
 }
 impl<'a> Page<'a> {
-    pub fn new(name: &'a str, x_label: &'a str, y_label: &'a str) -> Self {
+    pub fn new(name: &'a str, config: ContinuousView) -> Self {
         Page {
             plots: Vec::new(),
-            x_label,
-            y_label,
+            config,
             name
         }
     }
-    pub fn add_plot(mut self, plot: Plot<'a>) -> Self {
+    pub fn add_plot(mut self, plot: Plot) -> Self {
         self.plots.push(plot);
         self
     }
 
 
 }
-pub struct Plot<'a> {
-    x: Box<dyn Iterator<Item = f64> + 'a>,
-    y: Box<dyn Iterator<Item = f64> + 'a>,
+pub struct Plot {
+    x: Array1<f64>,
+    y: Array1<f64>,
     legend: Option<String>,
 }
-impl<'a> Plot<'a> {
-    pub fn new<I, J>(x: I, y: J) -> Plot<'a>
-        where I: Iterator<Item = f64> + 'a,
-              J: Iterator<Item = f64> + 'a
-    {
+impl Plot {
+    pub fn new(x: Array1<f64>, y: Array1<f64>) -> Plot {
         Plot {
-            x: Box::new(x),
-            y: Box::new(y),
-            legend: None
+            x,
+            y,
+            legend: None,
         }
     }
 
@@ -61,49 +60,42 @@ pub trait Experiment: Serialize + DeserializeOwned {
     /// plotting experiments in the same graph.
     /// `path` is the path to the directory.
     fn write_plot<'a>(experiments: &'a [Self], path: &str) -> Result<(), Error> {
-        const PRINT: bool = true;
+        println!("Writing to {}", path);
         let colors = vec!["olivedrab", "lightcoral", "royalblue", "peru", "darkcyan", "saddlebrown", "darkmagenta"];
         use plotlib::repr::{Line, LineStyle};
 
-        for mut page in Self::plot(experiments) {
+        for page in Self::plot(experiments) {
 
-            let mut view = plotlib::view::ContinuousView::new() // TODO maybe put this in `Page`
-                .x_label(page.x_label)
-                .y_label(page.y_label)
-                .y_range(0.0, 0.06)
-                .y_max_ticks(16);
+            let mut view = page.config;
 
-            for (i, plot) in page.plots.iter_mut().enumerate() {
+            for (i, plot) in page.plots.into_iter().enumerate() {
                 // let data: Vec<_> = Iterator::zip(plot.x, plot.y).map(|(x,y)| (*x as f64, *y)).collect();
-                let data: Vec<_> = plot.x.by_ref().zip(plot.y.by_ref()).collect();
-                let mut line = Line::new(data.clone())
-                    .style(LineStyle::new().colour(colors[i % colors.len()]));
+                let data: Vec<_> = Iterator::zip(plot.x.iter().cloned(), plot.y.iter().cloned()).collect();
+                let mut line = Line::new(data)
+                    .style(LineStyle::new().colour(colors[i % colors.len()]).width(1.5));
                 if let Some(ref legend) = plot.legend {
                     line = line.legend(legend.clone());
                 }
                 view = view.add(Box::new(line));
-                if PRINT {
-                    println!("= data =");
-                    for (x, y) in data {
-                        println!("  {}, {}", x, y);
-                    }
-                }
             }
 
+            println!(" - {}/{}.svg", path, page.name);
             plotlib::page::Page::single(&view)
                 .save(&format!("{}/{}.svg", path, page.name))?;
         }
         Ok(())
     }
+    // fn merge(&self, other: &Self) -> Self;
 
     /// Both saves and plots (calls `present`)
-    fn save(experiments: &[Self], path: &str) -> Result<(), Error>
+    fn save(experiments: Vec<Self>, path: &str) -> Result<(), Error>
     {
         let date = Local::now();
         let _ = fs::create_dir_all(path);
-        Self::write_plot(experiments, path)?;
+        Self::write_plot(&experiments, path)?;
         let out_file = fs::File::create(format!("{}/data.bincode", path))?;
-        bincode::serialize_into(out_file, experiments)?;
+        ron::ser::to_string(&experiments)
+        bincode::serialize_into(out_file, &experiments)?;
         Ok(())
     }
     /// `path` should be to a file `data.bincode`
